eae405a1d90db4146535644f663675c07b41e727
Augustė Bagdzevičiūtė <79446863+augustebag@users.noreply.github.com>
2021-05-31 20:57:21 +0300

    namu darbas

--- 02/nd.php
+++ 02/nd.php
@@ -83,19 +83,24 @@ echo '<br>'; echo '<br><hr>'; echo '<br>';
 
 // 5 uzdavinys
 
-$sk1 = rand(0, 2);
-$sk2 = rand(0, 2);
-$sk3 = rand(0, 2);
-$sk4 = rand(0, 2);
+$pirmas = rand(0, 2);
+$antras = rand(0, 2);
+$trecias = rand(0, 2);
+$ketvirtas = rand(0, 2);
 
-echo "Pirmas skaičius = $sk1";
-echo '<br>';
-echo "Antras skaičius = $sk2";
-echo '<br>';
-echo "Trečias skaičius = $sk3";
-echo '<br>';
-echo "Ketvirtas skaičius = $sk4";
-echo '<br>';
+echo "$pirmas, $antras, $trecias, $ketvirtas";
+echo '<br>'; echo '<br>';
+
+$counter0 = 0;
+$counter1 = 0;
+$counter2 = 0;
+
+($pirmas === 0) ? $counter0++ : ( ($pirmas === 1) ? $counter1++ : $counter2++ );
+($antras === 0) ? $counter0++ : ( ($antras === 1) ? $counter1++ : $counter2++ );
+($trecias === 0) ? $counter0++ : ( ($trecias === 1) ? $counter1++ : $counter2++ );
+($ketvirtas === 0) ? $counter0++ : ( ($ketvirtas === 1) ? $counter1++ : $counter2++ );
+
+echo "nulių yra - $counter0; vienetų yra - $counter1; dvejetų yra - $counter2";
 
 echo '<br>'; echo '<br><hr>'; echo '<br>';
 
@@ -108,16 +113,108 @@ echo '<br>'; echo '<br><hr>'; echo '<br>';
 
 // 7 uzdavinys
 
-$s1 = rand(-10, 10);
-$s2 = rand(-10, 10);
-$s3 = rand(-10, 10);
+$pirmas = rand(-10,10);
+$antras = rand(-10,10);
+$trecias = rand(-10,10);
 
-echo "Pirmas skaičius = $s1";
+$spalva1 = $pirmas < 0 ? 'green' : ($pirmas > 0 ? 'blue' : 'red');
+$spalva2 = $antras < 0 ? 'green' : ($antras > 0 ? 'blue' : 'red');
+$spalva3 = $trecias < 0 ? 'green' : ($trecias > 0 ? 'blue' : 'red');
+
+echo "Pirmas skaicius: <font style='color: $spalva1; font-size: 25px; font-weight: bold'>$pirmas </font>";
+echo "Antras skaicius: <font style='color: $spalva2; font-size: 25px; font-weight: bold'>$antras </font>";
+echo "Trecias skaicius: <font style='color: $spalva3; font-size: 25px; font-weight: bold'>$trecias </font>";
+
+echo '<br>'; echo '<br><hr>'; echo '<br>';
+
+// 8 uzdavinys
+
+$zvakiuKiekis = rand(5, 3000);
+
+if ($zvakiuKiekis<1000){
+    echo "kiekis: $zvakiuKiekis, nuolaida = 0, kaina: $zvakiuKiekis";
+}
+if ($zvakiuKiekis>= 1000 && $zvakiuKiekis<2000) {
+    echo 'kiekis: '. $zvakiuKiekis.' , nuolaida: '.$zvakiuKiekis * 0.03 . ' , kaina: '. $zvakiuKiekis - ($zvakiuKiekis * 0.03);
+}
+if ($zvakiuKiekis>= 2000) {
+    echo 'kiekis: '. $zvakiuKiekis. ' , nuolaida: '.$zvakiuKiekis * 0.04 .' , kaina: '. $zvakiuKiekis - ($zvakiuKiekis * 0.04);
+}
+
+echo '<br>'; echo '<br><hr>'; echo '<br>';
+
+// 9 uzdavinys
+
+$skaicius1 = rand(0, 100);
+$skaicius2 = rand(0, 100);
+$skaicius3 = rand(0, 100);
+
+echo "Skaiciai: $skaicius1 - $skaicius2 - $skaicius3";
 echo '<br>';
-echo "Antras skaičius = $s2";
+if ($skaicius1 < 90  && 10 < $skaicius1) {
+    $sk1 = "$skaicius1";
+  
+}
+else {
+    $sk1 = 0;
+   
+};
+if ($skaicius2 < 90  && 10 < $skaicius2) {
+    $sk2 = "$skaicius2";
+  
+}
+else {
+    $sk2 = 0;
+   
+};
+if ($skaicius3 < 90  && 10 < $skaicius3) {
+    $sk3 = "$skaicius3";
+  
+}
+else {
+    $sk3 = 0;
+   
+};
+$average = round(($sk1 + $sk2 + $sk3) / 3, 2);
+echo "Vidurkis: $average";
 echo '<br>';
-echo "Trečias skaičius = $s3";
 echo '<br>';
+
+
+echo '<br>'; echo '<br><hr>'; echo '<br>';
+
+// 10 uzdavinys
+
+$hours = rand(0, 23);
+$minutes = rand(0, 59);
+$seconds = rand(0, 59);
+
+$length = 2;
+
+$hours = substr(str_repeat(0, $length).$hours, -$length);
+$minutes = substr(str_repeat(0, $length).$minutes, -$length);
+$seconds = substr(str_repeat(0, $length).$seconds, -$length);
+
+echo "<h1>$hours : $minutes : $seconds</h1>";
+
+$extraSeconds = rand(0, 300);
+
+echo "Papildomos sekundės: $extraSeconds";
+echo '<br>';
+
+$totalTime = $seconds + $minutes * 60 + $hours * 3600 + $extraSeconds;
+
+$seconds = $totalTime % 60;
+$minutes = (($totalTime - $seconds) / 60) % 60 ;
+$hours = (($totalTime - $seconds) / 60 - $minutes) / 60;
+
+$hours = substr(str_repeat(0, $length).$hours, -$length);
+$minutes = substr(str_repeat(0, $length).$minutes, -$length);
+$seconds = substr(str_repeat(0, $length).$seconds, -$length);
+
+echo "<h1>$hours : $minutes : $seconds</h1>";
+
+echo '<br>'; echo '<br><hr>'; echo '<br>';
 
 // 11 uzdavinys
 
