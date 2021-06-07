82c75d6ac201d14c78f562fc13530db9ae58eec1
Augustė Bagdzevičiūtė <79446863+augustebag@users.noreply.github.com>
2021-06-07 16:20:26 +0300

    3-4

--- 06/masyvai1-4.php
+++ 06/masyvai1-4.php
@@ -5,8 +5,8 @@ echo '1 uzduotis'; echo '<br>';
 // Sugeneruokite masyvą iš 10 elementų, kurio elementai 
 // būtų masyvai iš 5 elementų su reikšmėmis nuo 5 iki 25.
 $masyvas = [];
-foreach (range(1, 10) as $masyvas1 => $_) {
-    foreach (range(1, 5) as $masyvas2 => $_) {
+foreach (range(1, 10) as $masyvas1 => $val1) {
+    foreach (range(1, 5) as $masyvas2 => $val2) {
         $masyvas[$masyvas1][$masyvas2] = rand(5, 25);
     }
 }
@@ -19,9 +19,9 @@ echo '2 uzduotis'; echo '<br>';
 // a. Suskaičiuokite kiek masyve yra elementų didesnių už 10;
 
 $daugiau10 = 0;
-foreach ($masyvas as $masyvas1) {
-    foreach ($masyvas1 as $masyvas2) {
-        if ($masyvas2 > 10) {
+foreach ($masyvas as $val1) {
+    foreach ($val1 as $val2) {
+        if ($val2 > 10) {
             $daugiau10++;
         }
     }
@@ -34,36 +34,49 @@ echo '<br><hr>'; echo '<br>';
 $didziausiasElementas = 0;
 foreach ($masyvas as $masyvas1) {
     foreach ($masyvas1 as $masyvas2) {
-        if ($masyvas2 > $didziausiasElementas) {
+        if ($val2 > $didziausiasElementas) {
             $didziausiasElementas = $masyvas2;
         }
     }
 }
+
 echo "Didžiausias elementas yra: $didziausiasElementas";
 echo '<br><hr>'; echo '<br>';
 
 // c. Suskaičiuokite kiekvieno antro lygio masyvų su vienodais 
 // indeksais sumas (t.y. suma reikšmių turinčių indeksą 0, 1 ir t.t.)
 
-$suma = 0;
+/* DESTYTOJO VARIANTAS
+$sums = [];
 
-// d. Visus masyvus “pailginkite” iki 7 elementų
-foreach ($masyvas as &$masyvas2) {
-    while(count($masyvas2) < 7) {
-        $masyvas2[] = rand(1, 5);
+foreach ($masyvas as $masyvas1 => $val1) {
+    foreach ($masyvas1 as $masyvas2 => $val2) {
+        $sums[$masyvas2] = ($sums[$masyvas2] ?? 0) + $val2;
     }
 }
-print_r($masyvas);
+
+print_r($sums);
+*/
+
+echo '<br><hr>'; echo '<br>';
+
+// d. Visus masyvus “pailginkite” iki 7 elementų
+
+foreach ($masyvas as $masyvas2 => &$value) {
+    $value[] =  rand(5, 25);
+    $value[] =  rand(5, 25);
+}
+print_r($value);
 echo '<br><hr>'; echo '<br>';
 
 // e. Suskaičiuokite kiekvieno iš antro lygio masyvų elementų sumą atskirai ir 
 // sumas panaudokite kaip reikšmes sukuriant naują masyvą. T.y. pirma naujo masyvo 
 // reikšmė turi būti lygi mažesnio masyvo, turinčio indeksą 0 dideliame masyve, visų elementų sumai 
- $masyvoSuma = [];
- foreach($masyvas as $masyvas2) {
+$masyvoSuma = [];
+foreach($masyvas as $masyvas2) {
      $masyvoSuma[] = array_sum($masyvas2);
- }
- print_r($masyvoSuma);
+}
+print_r($masyvoSuma);
 echo '<br><hr>'; echo '<br>';
 
 // 3 uzduotis
@@ -72,3 +85,30 @@ echo '3 uzduotis'; echo '<br>';
 // atsitiktiniu kiekiu nuo 2 iki 20 elementų. Elementų reikšmės atsitiktinai parinktos 
 // raidės iš intervalo A-Z. Išrūšiuokite antro lygio masyvus pagal abėcėlę (t.y. tuos kur su raidėm).
 
+$masyvas = [];
+$abc = range('A', 'Z');
+foreach (range(1, 10) as $keyBig => $_) {
+    foreach (range(1, rand(2, 20)) as $keySmall => $_) {
+        $masyvas[$keyBig][$keySmall] =$abc[rand(0, 25)];
+    }
+}
+print_r($masyvas); 
+echo '3 uzduotis'; echo '<br>';
+
+// 4 uzduotis
+echo '4 uzduotis'; echo '<br>';
+// RUSIAVIMAS
+
+_d($masyvas);
+usort($masyvas, function($a, $b) {
+    $ak =  (int)in_array('K', $a);
+    $bk =  (int)in_array('K', $a);
+
+    if ($ak + $ bk == 1) {
+        return $ak <=> $bk;
+    }
+
+    return count($a) <=> count($b);
+});
+
+print_r($masyvas);
