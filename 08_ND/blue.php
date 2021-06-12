b3dc1833ac052fd63649ed2a1b6607f5cac532aa
Augustė Bagdzevičiūtė <79446863+augustebag@users.noreply.github.com>
2021-06-12 21:18:08 +0300

    penktas

---
+++ 08_ND/blue.php
@@ -0,0 +1,23 @@
+<?php
+
+if(isset($_GET['color'])) {
+    if($_GET['color'] == 'red') {
+        header('Location: ./red.php');
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Penkta užduotis</title>
+</head>
+<body style="background-color: blue">
+    <h1 style="color: white">Penkta užduotis</h1>
+    <a href="?color=red">Debesėlis</a>
+</body>
+</html>
---
+++ 08_ND/red.php
@@ -0,0 +1,23 @@
+<?php
+
+if(isset($_GET['color'])) {
+    if($_GET['color'] == 'blue') {
+        header('Location: ./blue.php');
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Penkta užduotis</title>
+</head>
+<body style="background-color: red">
+    <h1 style="color: white">Penkta užduotis</h1>
+    <a href="?color=blue">Pomidoras</a>
+</body>
+</html>
