d2108e00efe32c75b8c7d8f87c96aa39a3b81f42
Augustė Bagdzevičiūtė <79446863+augustebag@users.noreply.github.com>
2021-06-13 19:17:49 +0300

    8

--- 08_ND/09.php
+++ 08_ND/09.php
@@ -1,19 +1,17 @@
-<?php
-
-if(empty($_POST)) {
-    header ('Location: http://localhost/barsukas/08_ND/08_pink.php');
-    die;
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aštunta užduotis</title>
+    <title>Devinta užduotis</title>
 </head>
-<body style="background-color:#FF0080">
+<body style="background-color: black">
+<h1 style="color: pink"> Devinta užduotis</h1>
+
+<form action="" method="post">
+
+</form>
+    
 </body>
 </html>
