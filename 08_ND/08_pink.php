444e8e2d0641a7e96134857d3211ce71ed5f4879
Augustė Bagdzevičiūtė <79446863+augustebag@users.noreply.github.com>
2021-06-13 15:13:35 +0300

    8

---
+++ 08_ND/08_pink.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aštunta užduotis</title>
+</head>
+    <body style="background-color: pink;">
+    <h1 style="color: white">Aštunta užduotis</h1>
+    
+    <form action="http://localhost/barsukas/08_ND/08_rose.php" method="post">
+    <button type="submit">GO TO ROSE</button>
+    </form>
+    
+</body>
+</html>
---
+++ 08_ND/08_rose.php
@@ -0,0 +1,19 @@
+<?php
+
+if(empty($_POST)) {
+    header ('Location: http://localhost/barsukas/08_ND/08_pink.php');
+    die;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aštunta užduotis</title>
+</head>
+<body style="background-color: red">
+</body>
+</html>
