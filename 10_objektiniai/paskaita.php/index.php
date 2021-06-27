fca72ea0d00b5edc5b7c1b846eadcacc80d2d4dd
Augustė Bagdzevičiūtė <79446863+augustebag@users.noreply.github.com>
2021-06-27 15:17:10 +0300

    paskaita

---
+++ 10_objektiniai/paskaita.php/index.php
@@ -0,0 +1,38 @@
+<?php
+
+// Programuotojas Petras
+
+
+require __DIR__ . '/Bebras.php';
+require __DIR__ . '/UpinisBebras.php';
+
+
+$bebras1 = new UpinisBebras('Blue'); // Priskyrimas turi buti su zodziu new
+$bebras2 = new UpinisBebras;
+$bebras3 = $bebras1;
+
+$bebras1->__A__(5);
+
+// echo '<pre>';
+// var_dump($bebras1);
+// echo '<br>';
+// var_dump($bebras2);
+// echo '<br>';
+// var_dump($bebras3);
+// echo '<br>';
+
+$bebras2->name = 'Bobikas'; // vietoj tasko, dedame rodykle (prideda veiksma)
+
+// echo $bebras1->randomNumber(1000);
+
+// echo $bebras1->getAge();
+
+// $bebras2->setAge(8);
+// $bebras2->dfgfgfdgfdgfd = 20;
+
+
+// $bebras2->addYear();
+// $bebras2->addYear();
+
+$bebras1->sayHello();
+$bebras2->sayHello();
