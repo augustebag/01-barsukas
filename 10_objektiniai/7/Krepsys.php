f2975c8caf61a9e5ef923523b08bac35315e411e
Augustė Bagdzevičiūtė <79446863+augustebag@users.noreply.github.com>
2021-07-06 23:42:30 +0300

    7

---
+++ 10_objektiniai/7/Krepsys.php
@@ -0,0 +1,14 @@
+<?php
+
+class Krepsys {
+
+    const DYDIS = 500;
+    private $pririnkta = 0;
+
+    public function deti(Grybas $grybas) {
+        if ($grybas->valgomas && !$grybas->sukirmijes) {
+            $this->pririnkta += $grybas->svoris;
+        }
+        return self::DYDIS > $this->pririnkta; // jei daugiau pririnkta yra true ir galime toliau rinkti
+    }
+}
