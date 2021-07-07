ff17a49987e636f0dd01afe96086bc3dca354f75
Augustė Bagdzevičiūtė <79446863+augustebag@users.noreply.github.com>
2021-07-07 23:35:44 +0300

    composer

--- 12/index.php
+++ 12/index.php
@@ -4,6 +4,20 @@ use Zoo\Animal as AnimalL;
 use Australia\Animal as AnimalA;
 use Belgium\Animal as AnimalB;
 
+require __DIR__ . '/vendor/autoload.php';
+
+// spl_autoload_register(function($class) {
+//     echo "Nerandu klases: $class";
+// });
+
+// spl_autoload_register(function($class) {
+//     echo "Nerandu klases: $class";
+// });
+
+// spl_autoload_register(function($class) {
+//     echo "Nerandu klases: $class";
+// });
+
 require __DIR__ . '/australai/Animal.php';
 require __DIR__ . '/belgai/Animal.php';
 require __DIR__ . '/Animal.php';
