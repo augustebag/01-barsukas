489700e9e380def2326a79d6b95ee16bc7c0d431
Augustė Bagdzevičiūtė <79446863+augustebag@users.noreply.github.com>
2021-07-11 09:46:04 +0300

    db

--- db/index.php
+++ db/index.php
@@ -1,3 +1,5 @@
+<?php
+
 $host = '127.0.0.1';
 $db   = 'baze';
 $user = 'root';
@@ -7,13 +9,24 @@ $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // kaip masyvas
     PDO::ATTR_EMULATE_PREPARES   => false,
 ];
-try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
+$pdo = new PDO($dsn, $user, $pass, $options);
 
-// Irasymas
+// Seeder
+$trees = [
+    ['Beržas', rand(0, 1500) / 100, 1],
+    ['Eglė', rand(0, 1500) / 100, 2],
+    ['Pušis', rand(0, 1500) / 100, 2],
+    ['Kedras', rand(0, 1500) / 100, 2],
+    ['Palmė', rand(0, 1500) / 100, 3],
+    ['Agrastas', rand(0, 1500) / 100, 2],
+    ['Šermukšnis', rand(0, 1500) / 100, 1],
+    ['Kaštonas', rand(0, 1500) / 100, 1],
+];
+$n = rand(0, count($trees) -1 );
+//Irasymas
 // INSERT INTO table_name (column1, column2, column3,...)
 // VALUES (value1, value2, value3,...)
 
